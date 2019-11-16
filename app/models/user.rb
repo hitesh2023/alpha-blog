e4491cdf87509290422c162a3953f5421eac4c42
Hitesh3202 <hitesh.sarangal@fourkites.com>
2019-11-16 16:58:43 +0530

    Validation Added for user Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
-	
+	validates :username, length: {minimum: 3, maximum: 25}, uniqueness: {case_sensitive: false}
+	validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: { case_sensitive: false}
 end
