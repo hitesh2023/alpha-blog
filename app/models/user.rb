4251307ae5d75a2794cb56b408fa2b84b8ab02a6
Hitesh3202 <hitesh.sarangal@fourkites.com>
2019-11-16 22:10:24 +0530

    Added Secure password and new column (password_digest) to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
 	before_save { self.email = email.downcase}
 	validates :username, length: {minimum: 3, maximum: 25}, uniqueness: {case_sensitive: false}
 	validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: { case_sensitive: false}
+
+	has_secure_password
 end
