327e752af063c552d03e82a4fe7069be0a7c071b
Hitesh3202 <hitesh.sarangal@fourkites.com>
2019-11-17 18:07:44 +0530

    Signup Users and singup Page Working ( both Frontend and Backend )

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
 	root 'home#index'
 	get 'about', to: 'home#about'
 	resources :articles
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+	get 'signup', to: 'users#new'
+	# resources :users, except: [:new]
+	post 'users' , to: 'users#create'
 end
