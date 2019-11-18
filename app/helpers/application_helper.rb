fa41e660e9884fd79bd4cee00472a27100002816
Hitesh3202 <hitesh.sarangal@fourkites.com>
2019-11-18 17:09:17 +0530

    Added User show action and Gravator Image fetching

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
+require 'digest/md5'
+
 module ApplicationHelper
+	def gravator_for(user, options = {size: 100})
+		email_address = user.email
+		size = options[:size]
+		hash = Digest::MD5.hexdigest(email_address)
+		image_src = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+		image_tag(image_src, alt: user.username, class: "img-circle")
+	end
 end
