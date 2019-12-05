4613b4e0f81b4d4b4c8a7bce4f81279b32ebfc8d
hitesh2023 <hiteshsarangal2023@gmail.com>
2019-12-05 19:42:21 +0530

    Update Navbar with the abiity to create new Category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ApplicationRecord
-	validates :name, presence: true, length: { minimum: 3, maximum: 20 }, uniqueness: true
+	validates :name, presence: true, length: { minimum: 3, maximum: 20 }, uniqueness: {case_sensitive: false}
 end
