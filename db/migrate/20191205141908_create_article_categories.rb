111f098c5b0bef379602f3ce59340d19bacae1bf
hitesh2023 <hiteshsarangal2023@gmail.com>
2019-12-08 14:42:22 +0530

    Category controller, views and Models working perfectly

--- db/migrate/20191205141908_create_article_categories.rb
+++ db/migrate/20191205141908_create_article_categories.rb
@@ -3,6 +3,7 @@ class CreateArticleCategories < ActiveRecord::Migration[5.2]
     create_table :article_categories do |t|
     	t.integer :article_id
     	t.integer :category_id
+    	t.timestamps
     end
   end
 end
